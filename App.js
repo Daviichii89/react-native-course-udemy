90e5b55271e80076e29cecf03d66577b40cf72fa
david.fernandez <david.fernandez@manomano.com>
2024-10-15 14:18:48 +0200

    Tested different navigations

--- App.js
+++ App.js
@@ -1,26 +1,62 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
+
+const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      <Text>List of goalsdsadasdsadsad...</Text>
-    </View>
+    <NavigationContainer>
+      {/* <Drawer.Navigator screenOptions={{
+        headerStyle: {backgroundColor: '#3c0a6b'},
+        headerTintColor: 'white',
+        drawerActiveBackgroundColor: '#f0e1ff',
+        drawerActiveTintColor: '#3c0a6b',
+        // drawerStyle: {backgroundColor: '#ccc'},
+      }}>
+        <Drawer.Screen
+          name='Welcome'
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: 'Welcome Screen',
+            drawerIcon: ({color, size}) => <Ionicons name='home' color={color} size={size} /> ,
+          }}
+        />
+        <Drawer.Screen
+          name='User'
+          component={UserScreen}
+          options={{
+            drawerLabel: 'User Screen',
+            drawerIcon: ({color, size}) => <Ionicons name='person' color={color} size={size} /> ,
+          }}
+        />
+      </Drawer.Navigator> */}
+      <BottomTab.Navigator screenOptions={{
+        headerStyle: {backgroundColor: '#3c0a6b'},
+        headerTintColor: 'white',
+        tabBarActiveTintColor: '#3c0a6b',
+      }}>
+        <BottomTab.Screen
+          name='Welcome'
+          component={WelcomeScreen}
+          options={{
+            tabBarIcon: ({color, size}) => <Ionicons name='home' color={color} size={size} />
+          }}
+        />
+        <BottomTab.Screen
+          name='User'
+          component={UserScreen}
+          options={{
+            tabBarIcon: ({color, size}) => <Ionicons name='person' color={color} size={size} />
+          }}
+        />
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  appContainer: {
-    padding: 50,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
-    marginRight: 8,
-    padding: 8
-  }
-});
---
+++ gesture-handler.native.js
@@ -0,0 +1,3 @@
+import './gesture-handler'
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
